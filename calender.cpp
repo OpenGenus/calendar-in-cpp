096e5c2ed7bd0172e6756c8169e01745c7dc15a5
aswins2108@gmail.com <aswins2108@gmail.com>
2023-02-21 07:39:48 +0530

    Added color grading to dates and months

--- calender.cpp
+++ calender.cpp
@@ -11,6 +11,16 @@ void printCalendar(int year)
     int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     string monthList[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
+    time_t ttime = time(0);
+    tm *local_time = localtime(&ttime);
+
+    int current_year = 1900 + local_time->tm_year;
+    int current_month = local_time->tm_mon + 1;
+    int current_day = local_time->tm_mday;
+    cout << current_year << endl;
+    cout << current_month << endl;
+    cout << current_day << endl;
+
     cout << " -----------------------------------" << endl;
     cout << "           Calendar - " << year << endl;
     cout << " -----------------------------------" << endl;
@@ -33,8 +43,24 @@ void printCalendar(int year)
         else
             days = monthDays[i];
 
-        cout << endl
-             << "  ------------" << monthList[i] << "-------------" << endl;
+        cout << endl;
+        if (year == current_year && current_month == i + 1)
+        {
+            cout << "\033[1;1;32m"
+                 << "  ------------" << monthList[i] << "-------------"
+                 << "\033[0m" << endl;
+        }
+        else if (year < current_year || (year == current_year && i + 1 < current_month))
+        {
+            cout << "\033[31m"
+                 << "  ------------" << monthList[i] << "-------------"
+                 << "\033[0m" << endl;
+        }
+        else
+        {
+            cout << "  ------------" << monthList[i] << "-------------"
+                 << endl;
+        }
 
         cout << "  Sun  Mon  Tue  Wed  Thu  Fri  Sat" << endl;
         int k;
@@ -44,7 +70,18 @@ void printCalendar(int year)
         for (int j = 1; j <= days; j++)
         {
             k++;
-            cout << setw(5) << j;
+            if (year == current_year && current_month == i + 1 && current_day == j)
+            {
+                cout << "\033[1;1;32m" << setw(5) << j << "\033[0m";
+            }
+            else if (year < current_year || (year == current_year && i + 1 < current_month) || (year == current_year && i + 1 == current_month && j < current_day))
+            {
+                cout << "\033[31m" << setw(5) << j << "\033[0m";
+            }
+            else
+            {
+                cout << setw(5) << j;
+            }
             if (k > 6)
             {
                 k = 0;
